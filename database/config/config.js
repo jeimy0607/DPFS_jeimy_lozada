5a9f02cf2c803087de710beb2689744da5dba0b9
jeimy0607 <jeimylozada847@gmail.com>
2026-01-25 20:18:53 -0500

    config

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,29 @@
 //Credenciales de acceso a la base de datos
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: 'root',
-    password: 'UnaClaveSegura123!',
-    database: 'ecommerce',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
   },
 };
